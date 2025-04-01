738623b64c27226617554b5829b627aacf8e8c6d
lipan <670339228@qq.com>
2025-04-01 16:02:02 +0800

    重新修改游戏

--- src/settings.3833f.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/StartScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"52d36",resources:"eb1c8",main:"d4a50"}};
